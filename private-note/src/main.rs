97586a48a74a6efff87d6c17a6aff6ff99abd465
Arman Auorbindo <arman8895884469@gmail.com>
2025-07-09 14:03:59 +0530

    private-note

---
+++ private-note/src/main.rs
@@ -0,0 +1,207 @@
+use rand::{rngs::StdRng, RngCore};
+use std::{fs, path::Path, sync::Arc};
+use tokio::time::{sleep, Duration};
+
+use miden_client::{
+    account::{
+        component::{BasicFungibleFaucet, BasicWallet, RpoFalcon512}, Account, AccountBuilder, AccountId, AccountStorageMode, AccountType
+    }, asset::{FungibleAsset, TokenSymbol}, auth::AuthSecretKey, builder::ClientBuilder, crypto::{FeltRng, SecretKey}, keystore::FilesystemKeyStore, note::{
+        Note, NoteAssets, NoteExecutionHint, NoteExecutionMode, NoteInputs, NoteMetadata, NoteRecipient, NoteRelevance, NoteScript, NoteTag, NoteType
+    }, rpc::{Endpoint, TonicRpcClient}, store::InputNoteRecord, transaction::{OutputNote, TransactionKernel, TransactionRequestBuilder}, Client, ClientError, Felt, Word
+};
+use miden_objects::{account::NetworkId, Hasher};
+
+// Helper to create a basic account
+async fn create_basic_account(
+    client: &mut Client,
+    keystore: FilesystemKeyStore<StdRng>,
+) -> Result<miden_client::account::Account, ClientError> {
+    let mut init_seed = [0_u8; 32];
+    client.rng().fill_bytes(&mut init_seed);
+
+    let key_pair = SecretKey::with_rng(client.rng());
+    let anchor_block = client.get_latest_epoch_block().await.unwrap();
+    let builder = AccountBuilder::new(init_seed)
+        .anchor((&anchor_block).try_into().unwrap())
+        .account_type(AccountType::RegularAccountUpdatableCode)
+        .storage_mode(AccountStorageMode::Private)
+        .with_component(RpoFalcon512::new(key_pair.public_key()))
+        .with_component(BasicWallet);
+    let (account, seed) = builder.build().unwrap();
+    client.add_account(&account, Some(seed), false).await?;
+    keystore
+        .add_key(&AuthSecretKey::RpoFalcon512(key_pair))
+        .unwrap();
+
+    Ok(account)
+}
+
+async fn create_basic_faucet(
+    client: &mut Client,
+    keystore: FilesystemKeyStore<StdRng>,
+) -> Result<miden_client::account::Account, ClientError> {
+    let mut init_seed = [0u8; 32];
+    client.rng().fill_bytes(&mut init_seed);
+    let key_pair = SecretKey::with_rng(client.rng());
+    let anchor_block = client.get_latest_epoch_block().await.unwrap();
+    let symbol = TokenSymbol::new("MID").unwrap();
+    let decimals = 8;
+    let max_supply = Felt::new(1_000_000);
+    let builder = AccountBuilder::new(init_seed)
+        .anchor((&anchor_block).try_into().unwrap())
+        .account_type(AccountType::FungibleFaucet)
+        .storage_mode(AccountStorageMode::Public)
+        .with_component(RpoFalcon512::new(key_pair.public_key()))
+        .with_component(BasicFungibleFaucet::new(symbol, decimals, max_supply).unwrap());
+    let (account, seed) = builder.build().unwrap();
+    client.add_account(&account, Some(seed), false).await?;
+    keystore
+        .add_key(&AuthSecretKey::RpoFalcon512(key_pair))
+        .unwrap();
+    Ok(account)
+}
+
+// Helper to wait until an account has the expected number of consumable notes
+pub async fn wait_for_note(
+    client: &mut Client,
+    account_id: &Account,
+    expected: &Note,
+) -> Result<(), ClientError> {
+    loop {
+        client.sync_state().await?;
+
+        let notes: Vec<(InputNoteRecord, Vec<(AccountId, NoteRelevance)>)> =
+            client.get_consumable_notes(Some(account_id.id())).await?;
+
+        let found = notes.iter().any(|(rec, _)| rec.id() == expected.id());
+
+        if found {
+            println!("✅ note found {}", expected.id().to_hex());
+            break;
+        }
+
+        println!("Note {} not found. Waiting...", expected.id().to_hex());
+        sleep(Duration::from_secs(3)).await;
+    }
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), ClientError>{
+
+    let endpoint = Endpoint::testnet();
+    let timeout_ms = 10_000;
+    let rpc_api = Arc::new(TonicRpcClient::new(&endpoint, timeout_ms));
+
+    let mut client = ClientBuilder::new()
+        .with_rpc(rpc_api)
+        .with_filesystem_keystore("./keystore")
+        .in_debug_mode(true)
+        .build()
+        .await?;
+
+    let sync_summary = client.sync_state().await.unwrap();
+    println!("Latest block: {}", sync_summary.block_num);
+
+    let keystore = FilesystemKeyStore::new("./keystore".into()).unwrap();
+
+    // Creating new accounts and faucet
+    println!("\n[STEP 1] Creating new accounts");
+    let alice_account = create_basic_account(&mut client, keystore.clone()).await?;
+    println!("Alice's account ID: {:?}", alice_account.id().to_bech32(NetworkId::Testnet));
+    let bob_account = create_basic_account(&mut client, keystore.clone()).await?;
+    println!("Bob's account ID: {:?}", bob_account.id().to_bech32(NetworkId::Testnet));
+
+    println!("\nDeploying a new fungible faucet.");
+    let faucet = create_basic_faucet(&mut client, keystore).await?;
+    println!("Faucet account ID: {:?}", faucet.id().to_bech32(NetworkId::Testnet));
+    client.sync_state().await?;
+
+    // Step 2: Mint tokens with P2ID for Alice
+    println!("\n[STEP 2] Mint tokens with P2ID");
+    let faucet_id = faucet.id();
+    let amount: u64 = 100;
+    let mint_amount = FungibleAsset::new(faucet_id, amount).unwrap();
+    let tx_request = TransactionRequestBuilder::new()
+        .build_mint_fungible_asset(
+            mint_amount,
+            alice_account.id(),
+            NoteType::Public,
+            client.rng(),
+        )
+        .unwrap();
+    let tx_exec = client.new_transaction(faucet.id(), tx_request).await?;
+    client.submit_transaction(tx_exec.clone()).await?;
+
+    let p2id_note = if let OutputNote::Full(note) = tx_exec.created_notes().get_note(0) {
+        note.clone()
+    } else {
+        panic!("Expected OutputNote::Full");
+    };
+
+    wait_for_note(&mut client, &alice_account, &p2id_note).await?;
+
+    // Alice consume the P2ID note
+    let consume_request = TransactionRequestBuilder::new()
+        .with_authenticated_input_notes([(p2id_note.id(), None)])
+        .build()
+        .unwrap();
+    let tx_exec = client
+        .new_transaction(alice_account.id(), consume_request)
+        .await?;
+    client.submit_transaction(tx_exec).await?;
+    client.sync_state().await?;
+
+    // Create custom note for Bob
+    println!("\n[STEP 3] Create custom note");
+    let assembler = TransactionKernel::assembler().with_debug_mode(true);
+    let code = fs::read_to_string(Path::new("./masm/private-note.masm")).unwrap();
+    let serial_num = client.rng().draw_word();
+    let note_script = NoteScript::compile(code, assembler).unwrap();
+    let note_inputs = NoteInputs::new(vec![bob_account.id().prefix().as_felt(), bob_account.id().suffix()]).unwrap();
+    let recipient = NoteRecipient::new(serial_num, note_script, note_inputs);
+    let tag = NoteTag::for_public_use_case(0, 0, NoteExecutionMode::Local).unwrap();
+    let metadata = NoteMetadata::new(
+        alice_account.id(),
+        NoteType::Public,
+        tag,
+        NoteExecutionHint::always(),
+        Felt::new(0),
+    )?;
+    let vault = NoteAssets::new(vec![mint_amount.into()])?;
+    let custom_note = Note::new(vault, metadata, recipient);
+    println!("note hash: {:?}", custom_note.id().to_hex());
+
+    let note_request = TransactionRequestBuilder::new()
+        .with_own_output_notes(vec![OutputNote::Full(custom_note.clone())])
+        .build()
+        .unwrap();
+    let tx_result = client
+        .new_transaction(alice_account.id(), note_request)
+        .await
+        .unwrap();
+    println!(
+        "View transaction on MidenScan: https://testnet.midenscan.com/tx/{:?}",
+        tx_result.executed_transaction().id()
+    );
+    let _ = client.submit_transaction(tx_result).await;
+    client.sync_state().await?;
+
+    let consume_custom_request = TransactionRequestBuilder::new()
+        .with_unauthenticated_input_notes([(custom_note, None)])
+        .build()
+        .unwrap();
+    let tx_result = client
+        .new_transaction(bob_account.id(), consume_custom_request)
+        .await
+        .unwrap();
+    println!(
+        "Consumed Note Tx on MidenScan: https://testnet.midenscan.com/tx/{:?} \n",
+        tx_result.executed_transaction().id()
+    );
+    println!("account delta: {:?}", tx_result.account_delta().vault());
+    let _ = client.submit_transaction(tx_result).await;
+
+
+    Ok(())
+}
